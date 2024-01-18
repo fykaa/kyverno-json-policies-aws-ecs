83da88e35b99d6d0b0c679746b39b276cb6f33cf
fykaa <faeka6@gmail.com>
2024-01-18 23:30:00 +0530

    firstcommit

---
+++ validate-ecs-container-insights-enabled/policy-test/terraform-code/provider.tf
@@ -0,0 +1,22 @@
+# Setting up the configuration for using Docker and AWS providers
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~>2.20.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configuring docker and AWS as providers
+provider "docker" {}
+
+provider "aws" {
+  region  = "us-west-1"
+}
+
